b03060f72f2f09da678476556bdcc5130e6071c8
rithesh99 <ritheshdavid@gmail.com>
2020-12-22 14:29:51 +0530

    Models

---
+++ models/book.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    publisher: {
+        type: String,
+        required: true
+    },
+    lend: {
+        type: String,
+        required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
---
+++ models/user.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+
+var userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
