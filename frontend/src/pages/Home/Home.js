18571adea0096c12a8697111f02341f387763653
rithesh99 <ritheshdavid@gmail.com>
2020-12-24 00:22:34 +0530

    home page and lend by user

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { getBooks, updateBookLend } from '../../components/books/helper'
 import Base from '../../shared/Base'
+import './Home.css'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { isAuthenticated } from '../../components/auth';
 
 function Home() {
+
+    const [books, setBooks] = useState([])
+    const [lend, setLend] = useState(false)
+    const {user, token} = isAuthenticated()
+
+    const onLend = (id) => {
+        // event.preventDefault()
+        updateBookLend(token,  id, user._id, {lend:user.name})
+        .then(res => console.log(res))
+        .then(res => setLend(!lend))
+        .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+       getBooks()
+       .then(res => setBooks(res))
+       .catch(err => console.log(err))
+       console.log(books);
+    }, [lend])
+
     return (
         <Base>
-            
+            <div className="search__field">
+                <input type="text"/>
+            </div>
+            <div className="home">
+                {books.map((book,i) => {
+                    return(
+                        <div className="card1">
+                            <div className="card__image">
+                                <img src={book.imageUrl} alt="" className="card__image__pic"/>
+                            </div>
+                            <div className="card__details">
+                                <p className="card__name">{book.name}</p>
+                                <p className="card__author">-{book.author}</p>
+                                <span className="card__category">{book.category}</span>
+                                <p className="card__publishedBy">Published By: {book.publisher}</p>
+                            
+                             </div>
+                             {book.lend == "none" ? (
+                                 <div className="card__lend">
+                                    <p className="card__lendBy">Available</p>
+                                    <a className="card__lendBy__icon"  ><ShoppingCartIcon onClick={() => onLend(book._id)} /></a>
+                                </div>
+                             ): (
+                                <div className="card__lend">
+                                    <p className="card__lendBy">Lended by: {book.lend}</p>
+                                </div>
+                             )}
+                            
+                        </div>  
+                    )
+                })}
+            </div>
         </Base>
     )
 }
